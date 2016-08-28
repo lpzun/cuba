70de3fde1113e55e76dc5fd1a117e514b16e2739
lpzun <peizunliu@gmail.com>
2016-08-28 19:46:02 -0400

    data structure definition

--- src/util/state.cc
+++ src/util/state.cc
@@ -15,14 +15,16 @@ namespace cuba {
 thread_config::thread_config() :
         s(), w() {
 }
+
 /**
  * A constructor with a control state and an alphabet
  * @param s
  * @param w
  */
-thread_config::thread_config(const control_state& s, const alphabet& w) :
+thread_config::thread_config(const control_state& s, const sstack& w) :
         s(s), w(w) {
 }
+
 /**
  * A constructor with a thread configuration
  * @param c
@@ -31,7 +33,39 @@ thread_config::thread_config(const thread_config& c) :
         s(c.get_state()), w(c.get_stack()) {
 }
 
+/**
+ * destructor
+ */
 thread_config::~thread_config() {
+
+}
+
+/**
+ * A constructor with a control state and the number of concurrent
+ * components
+ * @param s
+ * @param n
+ */
+global_config::global_config(const control_state& s, const size_t& n) :
+        s(s), W(n) {
+
+}
+
+/**
+ * A constructor with a control state and concurrent components
+ * @param s
+ * @param W
+ */
+global_config::global_config(const control_state& s, const cstack& W) :
+        s(s), W(W) {
+
+}
+
+/**
+ * destructor
+ */
+global_config::~global_config() {
+
 }
 
 } /* namespace bssp */
--- src/util/state.hh
+++ src/util/state.hh
@@ -20,20 +20,151 @@
 
 namespace cuba {
 
+/// the size of threads
+using size_t = unsigned short;
+
 /// control state definition
 using control_state = unsigned int;
 /// define stack symbol
 using stack_symbol = unsigned int;
-/// define input alphabet
-using alphabet = deque<stack_symbol>;
 
+template<typename T> class alphabet {
+public:
+    inline alphabet() :
+            worklist() {
+    }
+
+    inline alphabet(const deque<T>& worklist) :
+            worklist(worklist) {
+    }
+
+    inline ~alphabet() {
+    }
+
+    T top() {
+        return worklist.back();
+    }
+
+    void push(const T& _value) {
+        worklist.emplace_back(_value);
+    }
+
+    T pop() {
+        if (worklist.empty())
+            throw;
+        auto res = worklist.back();
+        worklist.pop_back();
+        return res;
+    }
+
+    void overwrite(const T& _value) {
+        if (worklist.empty())
+            throw;
+        worklist.pop_back();
+        worklist.push_back(_value);
+    }
+
+    bool empty() const {
+        return worklist.empty();
+    }
+
+    const deque<T>& get_worklist() const {
+        return worklist;
+    }
+
+private:
+    deque<T> worklist;
+};
+
+/**
+ * overloading operator <<: ostream
+ * @param os
+ * @param a
+ * @return ostream
+ */
+template<typename T> inline ostream& operator<<(ostream& os,
+        const alphabet<T>& a) {
+    for (const T& s : a.get_worklist())
+        os << s;
+    return os;
+}
+
+/**
+ * overloading operator <
+ * @param a1
+ * @param a2
+ * @return bool
+ */
+template<typename T> inline bool operator<(const alphabet<T>& a1,
+        const alphabet<T>& a2) {
+    if (a1.get_worklist().size() == a2.get_worklist().size()) {
+        auto ia1 = a1.get_worklist().cbegin();
+        auto ia2 = a2.get_worklist().cbegin();
+        while (ia1 != a1.get_worklist().cend()) {
+            if (*ia1 < *ia2) {
+                return true;
+            } else if (*ia1 > *ia2) {
+                return false;
+            } else {
+                ++ia1, ++ia2;
+            }
+        }
+        return false;
+    }
+    return a1.get_worklist().size() < a2.get_worklist().size();
+}
+
+/**
+ * overloading operator >
+ * @param a1
+ * @param a2
+ * @return bool
+ */
+template<typename T> inline bool operator>(const alphabet<T>& a1,
+        const alphabet<T>& a2) {
+    return a2 < a1;
+}
+
+/**
+ * overloading operator ==
+ * @param a1
+ * @param a2
+ * @return bool
+ */
+template<typename T> inline bool operator==(const alphabet<T>& a1,
+        const alphabet<T>& a2) {
+    if (a1.get_worklist().size() != a2.get_worklist().size())
+        return false;
+    auto iter1 = a1.get_worklist().cbegin();
+    auto iter2 = a2.get_worklist().cbegin();
+    while (iter1 != a1.get_worklist().cend()) {
+        if (*iter1 != *iter2)
+            return false;
+        ++iter1, ++iter2;
+    }
+    return true;
+}
+
+/**
+ * overloading operator !=
+ * @param a1
+ * @param a2
+ * @return bool
+ */
+template<typename T> inline bool operator!=(const alphabet<T>& a1,
+        const alphabet<T>& a2) {
+    return !(a1 == a2);
+}
+
+/// the stack for a single PDS
+using sstack = alphabet<stack_symbol>;
 /**
  * a configuration (s, w) of a PDS is an element of QxL*.
  */
 class thread_config {
 public:
     thread_config();
-    thread_config(const control_state& s, const alphabet& w);
+    thread_config(const control_state& s, const sstack& w);
     thread_config(const thread_config& c);
     ~thread_config();
 
@@ -41,13 +172,13 @@ public:
         return s;
     }
 
-    const alphabet& get_stack() const {
+    const sstack& get_stack() const {
         return w;
     }
 
 private:
     control_state s; /// control state
-    alphabet w; /// stack content
+    sstack w; /// stack content
 };
 
 /**
@@ -57,18 +188,92 @@ private:
  * @param c
  * @return ostream
  */
-inline ostream& operator<<(ostream& out, const thread_config& c) {
-    out << "(" << c.get_state() << "," << c.get_stack() << ")";
-    return out;
+inline ostream& operator<<(ostream& os, const thread_config& c) {
+    os << "(" << c.get_state() << "," << c.get_stack() << ")";
+    return os;
 }
 
-inline/**
+/**
+ * overloading operator <
+ * @param c1
+ * @param c2
+ * @return bool
+ */
+inline bool operator<(const thread_config& c1, const thread_config& c2) {
+    if (c1.get_state() == c2.get_state())
+        return c1.get_stack() < c2.get_stack();
+    return c1.get_state() < c2.get_state();
+}
+
+/**
+ * overloading operator >
+ * @param c1
+ * @param c2
+ * @return bool
+ */
+inline bool operator>(const thread_config& c1, const thread_config& c2) {
+    return c2 < c1;
+}
+
+/**
+ * overloading operator ==
+ * @param c1
+ * @param c2
+ * @return bool
+ */
+inline bool operator==(const thread_config& c1, const thread_config& c2) {
+    return (c1.get_state() == c2.get_state())
+            && (c1.get_stack() == c2.get_stack());
+}
+
+/**
+ * overloading operator !=
+ * @param c1
+ * @param c2
+ * @return bool
+ */
+inline bool operator!=(const thread_config& c1, const thread_config& c2) {
+    return !(c1 == c2);
+}
+
+/// the set of stacks in CPDS
+using cstack = vector<sstack>;
+
+/**
  * a configuration (s|w1,...,wn) of a CPDS is an element of Qx(L*)^n
  */
 class global_config {
 public:
+    global_config(const control_state& s, const size_t& n);
+    global_config(const control_state& s, const cstack& W);
+    ~global_config();
 
+    control_state get_state() const {
+        return s;
+    }
+
+    const cstack& get_stacks() const {
+        return W;
+    }
+
+private:
+    control_state s;
+    cstack W;
 };
+
+/**
+ * overloading operator <<: print a configuration of the CPDS
+ * @param os
+ * @param c
+ * @return bool
+ */
+inline ostream& operator<<(ostream& os, const global_config& c) {
+    os << "(" << c.get_state() << "|";
+    for (const auto& w : c.get_stacks())
+        os << w << ",";
+    os << ")";
+    return os;
+}
 
 } /* namespace bssp */
 
