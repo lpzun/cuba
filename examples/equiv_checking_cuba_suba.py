043b12f07690bafe09f6df938cb12dd1715d486e
lpzun <peizunliu@gmail.com>
2019-04-25 21:49:15 -0400

    matching information

---
+++ examples/equiv_checking_cuba_suba.py
@@ -0,0 +1,5 @@
+#####################################################
+# This script is for equivalence checking between 
+# (1) SUBA, the implemenation from Andrew Dai, and
+# (2) CUBA. 
+#####################################################
